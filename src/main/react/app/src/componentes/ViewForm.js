7128c0a297c1ec897165d5a61c6c4b36ad516cd1
Nullbylatolk <tlatolk.aguilar@gmail.com>
2023-08-23 20:13:03 -0600

    formulario del servicio request

--- src/main/react/app/src/componentes/ViewForm.js
+++ src/main/react/app/src/componentes/ViewForm.js
@@ -1,38 +1,79 @@
-import React ,{Container, Form, FormGroup, Label, Col, Input, FormText, Button, Row} from "reactstrap";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import React ,{Container, Form, FormGroup, Label, Col, Input, FormText, Button, Row, Alert} from "reactstrap";
 
 
 function  ViewForm (){
+	
+	const [message, setMessage] = useState("");
+	const navigate = useNavigate();
+	
+	
+	function createRequest(event){
+	event.preventDefault();
+	
+	console.log(event);
+	
+	const endPoint = event.target.action;
+	const data = Array.from(event.target.elements)
+			.filter((input) => input.name)
+			.reduce((obj, input) => Object.assign(obj, {[input.name]: input.value}), {});
+			
+			fetch(endPoint, {
+				method: 'POST',
+				headers:{
+					Accept: 'application/json',
+					  'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			}).then((response)=> {
+				
+				if(response.status !== 200){
+					throw new Error(response.statusText);
+				}
+				return response.json();
+			}).then(()=>{
+				navigate("/request");
+			}).catch((err)=> {
+				setMessage(err.toString());
+			});
+	
+		
+	}
+	
+	
 	return(
 	<Container className="bg-light border mt-5"    fluid = "md"    xs="auto">
 	
 	  <Row xs="2">
   <Col className="bg-light border">
-<Form>
+  
+  {message && <Alert color="danger"> {message}</Alert>}
+<Form action='/api/v2/requests/' method="post" onSubmit={createRequest}>
   <FormGroup>
-    <Label for="exampleEmail">
+    <Label for="inputNotice">
       Notice
     </Label>
     <Input
-      id="exampleEmail"
-      name="email"
-      placeholder="with a placeholder"
-      type="email"
+      id="inputNotice"
+      name="serviceNotice"
+      placeholder="write Notice"
     />
   </FormGroup>
   
  
   
   <FormGroup>
-    <Label for="exampleSelect">
+    <Label for="serviceName">
       Service
     </Label>
     <Input
-      id="exampleSelect"
-      name="select"
+      id="serviceName"
+      name="serviceName"
       type="select"
     >
-      <option>
-        1
+      <option value={7}>
+        7
       </option>
       <option>
         2
@@ -50,45 +91,43 @@ function  ViewForm (){
   </FormGroup>
   
   <FormGroup>
-    <Label for="exampleEmail">
+    <Label for="addressString">
       Address
     </Label>
     <Input
-      id="exampleEmail"
-      name="email"
-      placeholder="with a placeholder"
-      type="email"
+      id="addressString"
+      name="addressString"
+      placeholder="write address"
     />
   </FormGroup>
   
  
   
   <FormGroup>
-    <Label for="exampleText">
+    <Label for="requestDescription">
       Description
     </Label>
     <Input
-      id="exampleText"
-      name="text"
+      id="requestDescription"
+      name="requestDescription"
       type="textarea"
     />
   </FormGroup>
  
    <FormGroup>
-    <Label for="exampleEmail">
+    <Label for="mediaUrl">
       URL
     </Label>
     <Input
-      id="exampleEmail"
-      name="email"
-      placeholder="with a placeholder"
-      type="email"
+      id="mediaUrl"
+      name="mediaUrl"
+      placeholder="URL"
     />
   </FormGroup>
   
  
  
-  <Button>
+  <Button >
     Submit
   </Button>
 </Form>
